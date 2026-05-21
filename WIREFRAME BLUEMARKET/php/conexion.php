1ce5cd4cf6545b9a9d4ed7c6fc33821dc0240903
Jesus_Granados <jesusgranados3004@gmail.com>
2026-05-20 19:32:03 -0500

    Update conexion.php

--- WIREFRAME BLUEMARKET/php/conexion.php
+++ WIREFRAME BLUEMARKET/php/conexion.php
@@ -1,19 +1,16 @@
 <?php
-$host = "shinkansen.proxy.rlwy.net";
+$host = "localhost";
 $usuario = "root";
-$password = "HeYMRyxPvRNWFgGDGoBSuLkXwMLhLLLx";
-$bd = "railway";
-$puerto = "56697";
+$password = "3004";
+$bd = "pesca";
+$puerto = "3306";   
 
 $conexion = new mysqli($host, $usuario, $password, $bd, $puerto);
 
-// Verificar conexión
+
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
 
-// Configurar caracteres UTF-8
 $conexion->set_charset("utf8");
-
-echo "Conexión exitosa a Railway";
 ?>
