ae269df7c70b2f61d90f76e026c4334586894a27
Jesus_Granados <jesusgranados3004@gmail.com>
2026-05-20 20:11:51 -0500

    Update index.js

--- pescadirecta/js/index.js
+++ pescadirecta/js/index.js
@@ -4,7 +4,7 @@ let stockMaximo = 0;
 let todosLosProductos = [];
 
 // === CARGAR PRODUCTOS ===
-fetchSeguro("php/producto.php?origen=inicio")
+fetchSeguro("https://pesca-mcl1.onrender.com/conexion.php")
     .then(res => res.json())
     .then(data => {
         if (data.error) {
